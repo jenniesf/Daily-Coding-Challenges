7c3dbcc19dc5f0cfaac3817447561d56d4c74708
Jennie <99220339+jenniesf@users.noreply.github.com>
2022-05-08 14:03:04 -0700

    Add files via upload

---
+++ codewars_5.08.22.js
@@ -0,0 +1,9 @@
+        // CRASH OVERRIDE
+function aliasGen( x, y){
+
+        let firstLetter = x[0].toUpperCase()
+        let secondLetter = y[0].toUpperCase()
+
+        return  !firstName[firstLetter] ||  !surname[secondLetter] ? "Your name must start with a letter from A - Z." : firstName[firstLetter] + ' ' +surname[secondLetter]
+        
+}
