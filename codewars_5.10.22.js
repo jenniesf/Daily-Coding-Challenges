bdf9157e24f8f84e9bc23509878b90544336bce3
Jennie <99220339+jenniesf@users.noreply.github.com>
2022-05-11 18:37:59 -0700

    Add files via upload

---
+++ codewars_5.10.22.js
@@ -0,0 +1,11 @@
+        // ADDING ARRAYS
+function arrAdder(arr) {
+        let sentence = ""
+        for (let i = 0; i < arr[0].length ; i++) {
+                for( let j = 0 ; j < arr.length; j++ ){              
+                        sentence = sentence + arr[j][i]   
+                }
+        sentence = sentence + ' '
+        }
+        return sentence
+}
