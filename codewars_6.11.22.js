b084b0a17c99ff751462982f22805522f27ce6af
Jennie <99220339+jenniesf@users.noreply.github.com>
2022-06-11 17:23:16 -0700

    add

---
+++ codewars_6.11.22.js
@@ -0,0 +1,15 @@
+// Basic Mathematical Operations
+
+function basicOp(operation, value1, value2) {
+        return eval( value1 + operation + value2 ) 
+}
+
+//      if ( operation == '+') {
+//              return value1 + value2
+//      } else if( operation == '-') {
+//         return value1 - value2
+//      } else if( operation == '*') {
+//         return value1 * value2
+//      } else if( operation == '/') {
+//         return value1 / value2
+//      }    
